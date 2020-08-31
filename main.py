14c8216c00249c63630abe4cee557cc0d6bcdee9
hibarix <HibariX@163.com>
2020-08-31 15:01:37 +0800

    修复无法修改居住地址bug

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import re
 import json
 import argparse
 
+# get arguments
 parser = argparse.ArgumentParser(description='自动填写健康表')
 parser.add_argument('--username', '-u', help='用户名', required=True)
 parser.add_argument('--password', '-p', help='密码', required=True)
@@ -19,14 +20,13 @@ xjzdz = args.todayaddress       # 当天居住地址
 
 session = requests.Session()
 
-# user setting
-form_data = {"entity": {}}
-# get html
-url = "https://authserver.jluzh.com/cas/login?service=https%3A%2F%2Fwork.jluzh.com%2Fdefault%2Fwork%2Fjlzh%2Fjkxxtb%2Fjkxxcj.jsp%3Fappload%3D0%26f%3Dapp"
+# login(post)
+# -- get html
+url = "https://authserver.jluzh.com/cas/login?service=https%3A%2F%2Fwork.jluzh.com%2Fdefault%2Fwork%2Fjl" \
+      "zh%2Fjkxxtb%2Fjkxxcj.jsp%3Fappload%3D0%26f%3Dapp"
 response = session.get(url=url)
 html = response.text
-
-# get execution para
+# -- get execution para
 execution_re = re.findall('name="execution" value=".*/>', html)
 if len(execution_re) > 0:
     execution = execution_re[0].replace(
@@ -35,8 +35,6 @@ if len(execution_re) > 0:
 else:
     print('execution para is none')
     exit()
-
-# init data
 data = {
     'username': username,
     'password': password,
@@ -46,11 +44,11 @@ data = {
     'submit': "登  录"
 }
 
-# login(post)
 response = session.post(url=url, data=data)
 print(f"login status code: {response.status_code}")
 
-# test is login success ?
+
+# query base info
 url_temp = "https://work.jluzh.com/default/work/jlzh/jkxxtb/com.sudytech.portalone.base.db.queryBySqlWithoutPagecond.biz.ext"
 data = {
     'params': {
@@ -60,21 +58,56 @@ data = {
 
 }
 session.headers['content-type'] = 'application/json'        # Request Payload
+
 response = session.post(url=url_temp, data=json.dumps(data))
 query_json = response.json()
-print("query", response.text)
-form_data['entity'] = query_json
+list_json = query_json.get("list", [{}])[0]
+print("list_json", list_json)
+
 
 # submit form(post)
-form_data['entity']["__type"] = "sdo:com.sudytech.work.jlzh.jkxxtb.jkxxcj.TJlzhJkxxtb"
-form_data['entity']["_ext"] = "{}"
-form_data['entity']['jtdz'] = jtdz
-form_data['entity']['xjzdz'] = xjzdz
+form_data = {
+	'entity':{
+		'bjmc': list_json['BJMC'],
+		'bt': list_json['BT'],
+		'bz': list_json['BZ'],
+		'cn': ["本人承诺登记后、到校前不再前往其他地区"],
+		'fdygh': list_json['FDYGH'],
+		'fdymc': list_json['FDYMC'],
+		'gh': list_json['GH'],
+		'grjkzk': list_json['GRJKZK'],
+		'id': list_json['ID'],
+		'jkqk': list_json['JKQK'],
+		'jqqx': list_json['JQQX'],
+		'jrtw': list_json['JRTW'],
+		'jtdz': jtdz,
+		'lxdh': list_json['LXDH'],
+		'nj': list_json['NJ'],
+		'qsjkzk': list_json['QSJKZK'],
+		'rysf': "2",
+		'sfjchbjry': "否",
+		'sfjwhy': "否",
+		'sfjwhygjdq': "",
+		'sfqwhb': "否",
+		'sqbmid': list_json['SQBMID'],
+		'sqbmmc': list_json['SQBMMC'],
+		'sqrid': list_json['SQRID'],
+		'sqrmc': list_json['SQRMC'],
+		'ssh': list_json['SSH'],
+		'tbrq': list_json['TBRQ'],
+		'tjsj': list_json['TJSJ'],
+		'xb': "1",
+		'xjzdz': xjzdz,
+		'xrywz': "2",
+		'zymc': list_json['ZYMC'],
+		'__type': "sdo:com.sudytech.work.jlzh.jkxxtb.jkxxcj.TJlzhJkxxtb",
+		'_ext': "{}"
+	}
+}
 url = "https://work.jluzh.com/default/work/jlzh/jkxxtb/com.sudytech.portalone.base.db.saveOrUpdate.biz.ext"
 
 response = session.post(url=url, data=json.dumps(form_data))
 print(response.text)
-
 result = response.json()["result"]
 if result == "1":
     print('提交成功')
